045b4cd4d2bd3d780c530906bf5a3fdbc33f2870
RTheodoro4201 <rodrigotg2123@outlook.com>
2026-05-19 12:29:03 -0300

    feat: adiciona classe utilitária ErrorMessages para mensagens de erro padrão

---
+++ src/main/java/io/github/rtheodoro4201/task_api/utils/ErrorMessages.java
@@ -0,0 +1,8 @@
+package io.github.rtheodoro4201.task_api.utils;
+
+public class ErrorMessages {
+    private  ErrorMessages() {}
+
+    public static final String TASK_NOT_FOUND_MESSAGE = "Tarefa não encontrada.";
+    public static final String TASK_ALREADY_EXISTS_MESSAGE = "Já existe uma tarefa com este título e data.";
+}
